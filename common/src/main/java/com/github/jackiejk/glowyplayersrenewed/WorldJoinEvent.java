19457e1d957b2a99cacf7313ab9f940d548b36b5
Barrier <zeqide@163.com>
2024-08-06 21:45:21 +0800

    fabric实现

--- common/src/main/java/com/github/jackiejk/glowyplayersrenewed/WorldJoinEvent.java
+++ common/src/main/java/com/github/jackiejk/glowyplayersrenewed/WorldJoinEvent.java
@@ -1,7 +1,16 @@
 package com.github.jackiejk.glowyplayersrenewed;
 
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+
 public class WorldJoinEvent {
 
+  public static void onSpawn(ServerLevel level, Entity entity) {
+    if (level.isClientSide()) return;
+    if (!(entity instanceof Player player)) return;
+    player.setGlowingTag(true);
+  }
 
 }
--- fabric/src/main/java/com/github/jackiejk/glowyplayersrenewed/GlowyPlayersRenewed.java
+++ fabric/src/main/java/com/github/jackiejk/glowyplayersrenewed/GlowyPlayersRenewed.java
@@ -1,17 +1,17 @@
 package com.github.jackiejk.glowyplayersrenewed;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
 
 public class GlowyPlayersRenewed implements ModInitializer {
 
   @Override
   public void onInitialize() {
 
-    // This method is invoked by the Fabric mod loader when it is ready
-    // to load your mod. You can access Fabric and Common code in this
-    // project.
-
-    // Use Fabric to bootstrap the Common mod.
-    CommonClass.init();
+    ServerEntityEvents.ENTITY_LOAD.register(
+            (Entity entity, ServerLevel level) -> WorldJoinEvent.onSpawn(level, entity));
   }
+
 }
